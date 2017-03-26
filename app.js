c42a731b3aa59dc9e3516f3a255ec9884f1d91e4
leah <lperry07@mylangara.bc.ca>
2017-03-26 01:34:24 +0000

    commit message

--- app.js
+++ app.js
@@ -1,54 +1,177 @@
 
-// let myLat=[];
-// let myLng=[];
-
-// function GetLocation(){
-//     let geocoder=new google.maps.Geocoder();
-// let address=document.getElementById("address").value;
-
-// geocoder.geocode( { address:'address'}, function (results, status){
-//     if (status == google.maps.GeocoderStatus.OK){
-//         let latitude = results[0].geometry.location.lat();
-//         let longitude = results[0].geometry.location.lng();
-//         console.log(latitude, longitude);
-//     myLat.push(latitude);
-//     myLng.push(longitude);
-//     alert(latitude+longitude);
-//     } else {
-//         alert("error"+status);}
-        
-//     });
-
-// }
-
-
-
-
-
-// let myLocation=[];
-const myLatitude=[];
-const myLongitude=[];
-
-function getLocation(){
-
-const city = document.getElementById('city').value;
-const country = document.getElementById('country').value;
-
 const url='https://locationiq.org/v1/search.php?key=';
-const apiKey='b4f1a2a9d14fb5186eb9';
+const apiKey=' ';
+const btn=document.getElementById("btn");
 
- fetch(`${url}${apiKey}&format=json&city=${city}&country=${country}`)
- .then((res)=>res.json())
- .then((data)=>{
-     console.log(data);
-     console.log(`latitude:${data[0].lat} longitude: ${data[0].lon}`);
-     const lat=data[0].lat;
-     const lon=data[0].lon;
-     myLatitude.push(lat);
-     myLongitude.push(lon);
-})
-.catch((e) => console.log(e,"error"));
+btn.addEventListener('click',(e)=>{
+e.preventDefault();
+
+const cit = document.getElementById('city').value;
+const con = document.getElementById('country').value;
+
+getLocation(cit, con);
+});
+
+
+function getLocation(cit, con) {
+    
+  fetch(`${url}${apiKey}&format=json&city=${cit}&country=${con}`)
+    .then((res) => res.json())
+    .then((data) => {     
+        console.log(data);
+         const lat=(data[0].lat);
+         const long=(data[0].lon);
+          
+          const x= JSON.stringify(lat);
+         localStorage.setItem('lat', x);
+        const y = JSON.stringify(long);
+         localStorage.setItem('long', y);
+         
+        getWeather();
+    })
+.catch((e) => {
+    console.log(e,"error");
+    alert('We could not find the location you requested, is it spelled correctly?');
+});
 }
 
-console.log(myLatitude);
-console.log(myLongitude);
+
+
+
+function getWeather (){
+
+
+const Lat= localStorage.getItem('lat');
+const Latitude=JSON.parse(Lat);
+const Long = localStorage.getItem('long');
+const Longitude = JSON.parse(Long);
+const urlz='https://api.wunderground.com/api/APIKEY/conditions/q/'+Latitude+','+Longitude+'.json';
+let temperature=document.getElementById("temp");
+let describe=document.getElementById("desc");
+         
+  function createNode(element){
+         return document.createElement(element);
+     }
+     function append(parent, el){
+         return parent.appendChild(el);
+     }
+     
+ fetch(`${urlz}`)
+ .then((res)=>res.json())
+ .then((results)=>{
+         console.log(results);
+         let temp=(results.current_observation.temp_c);
+         let weather=(results.current_observation.weather);
+         let time=(results.current_observation.observation_time);
+         console.log(temp);
+         console.log(weather);
+
+         let p=createNode('p');
+         let span=createNode('span');
+         
+             describe.innerHTML=' ';
+             temperature.innerHTML=' ';
+
+         span.innerHTML=temp;
+         append(temperature, span);
+         p.innerHTML=weather; 
+         append(describe, p);
+         
+          if (weather===""){
+            let txt="Sorry this data is not available";
+            p.innerHTML=txt;
+            append(describe,p);
+          }
+          
+          let description=JSON.stringify(weather);
+         localStorage.setItem('description', description);
+         let main=JSON.stringify(temp);
+         localStorage.setItem('temp', main);
+         let timestamp=JSON.stringify(time);
+         localStorage.setItem('time', timestamp);
+         
+         appendImg();
+})
+.catch((e) => { console.log(e,"error");
+alert('No weather data found');
+});
+}
+ 
+
+
+
+function appendImg () {
+    
+    const cit = document.getElementById('city').value;
+    const con = document.getElementById('country').value;
+
+  cit.value=' ';
+  con.value=' ';
+  
+    const pic=document.getElementById("imgdiv"); 
+    let img=document.createElement("img");
+    
+  pic.innerHTML=' ';
+   
+   
+   const timestamp=localStorage.getItem('time');
+    let time=JSON.parse(timestamp);
+    const space = ' ';
+    const localTime = time.split(space);
+    console.log(localTime);
+    const x=':';
+    const hour=localTime[5].split(x);
+    console.log(hour[0]);
+ 
+    const description=localStorage.getItem('description');
+    let weather=JSON.parse(description);
+
+
+  if (weather.indexOf('Rain'||'Drizzle') >= 0) {
+      pic.appendChild(img);
+      img.src="imgs/rain.svg";
+  } 
+  else if (weather.indexOf('Clear'||'Sunny') >= 0) {
+      if (localTime[6]=='AM' & hour[0]>=7){
+          pic.appendChild(img);
+      img.src="imgs/sunny.svg";
+      } else if (localTime[6]=='PM' & hour[0]<=8) {
+          pic.appendChild(img);
+      img.src="imgs/sunny.svg";
+      } 
+      else {
+      pic.appendChild(img);
+      img.src="imgs/clearnight.svg";
+  }
+ } 
+ else if (weather.indexOf('Overcast'||'Cloudy'||'Partly Cloudy'||'Clouds') >= 0) {
+      if (localTime[6]=='AM' & hour[0]>=7 ){ 
+          pic.appendChild(img);
+      img.src="imgs/cloudy.svg";
+      }else if (localTime[6]=='PM' & hour[0]<=8){ 
+          pic.appendChild(img);
+      img.src="imgs/cloudy.svg";
+      }  else {
+ pic.appendChild(img);
+      img.src="imgs/night.svg";
+  }
+ } else if (weather.indexOf('Snow') >= 0) {
+    pic.appendChild(img);
+      img.src="imgs/snow.svg";
+  } else {
+      if (localTime[6]=='AM' & hour[0]>=7 ){
+          pic.appendChild(img);
+      img.src="imgs/overcast.svg";
+      }else if (localTime[6]=='PM' & hour[0]<=8){
+         pic.appendChild(img);
+      img.src="imgs/overcast.svg";
+      }else{
+      pic.appendChild(img);
+      img.src="imgs/night.svg";
+      }
+  }
+  localStorage.clear();
+}
+
+
+
